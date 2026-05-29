197c686b87131a6ae10ee4c7426c0d5d4f80d1b2
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-29 12:18:26 +0100

    minor changes

--- string.py
+++ string.py
@@ -9,19 +9,19 @@ reverse_second_word = second_word[::-1]
 print(reverse_second_word)
 
 # string methods
-text = "hello world"
-# text.upper() # converts to upper case
-# text.lower() # converts to lower case
-# text.strip() # removes whitespaces
-# text.replace(old, new) # replaces old with new
-# text.split(seperator) # splits into list
-# text.startswith(prefix) 
-# text.endswith(suffix) # returns boolean
-# text.find(char) # returns idx of first occurence
-# text.count(char) # counts total char occurence
-# text.isupper() # checks if all char is uppercase
-# text.islower() # checks if all char is lowercase
-# text.capitalize() # makes first letter uppercase
-# text.title() # makes first letter of each word uppercase
+text = "Hello world"
+text.upper() # converts to upper case
+text.lower() # converts to lower case
+text.strip() # removes whitespaces
+text.replace(old, new) # replaces old with new
+text.split(seperator) # splits into list
+text.startswith(prefix) 
+text.endswith(suffix) # returns boolean
+text.find(char) # returns idx of first occurence
+text.count(char) # counts total char occurence
+text.isupper() # checks if all char is uppercase
+text.islower() # checks if all char is lowercase
+text.capitalize() # makes first letter uppercase
+text.title() # makes first letter of each word uppercase
 
-print(text.title())
+print(text.capitalize())
