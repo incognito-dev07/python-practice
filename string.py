5d52906242e405a024f0685f3bc46f68eb686ef8
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-29 11:54:31 +0100

    minor changes

--- string.py
+++ string.py
@@ -9,7 +9,7 @@ reverse_second_word = second_word[::-1]
 print(reverse_second_word)
 
 # string methods
-text = "Hello world"
+text = "hello world"
 # text.upper() # converts to upper case
 # text.lower() # converts to lower case
 # text.strip() # removes whitespaces
@@ -19,7 +19,9 @@ text = "Hello world"
 # text.endswith(suffix) # returns boolean
 # text.find(char) # returns idx of first occurence
 # text.count(char) # counts total char occurence
+# text.isupper() # checks if all char is uppercase
+# text.islower() # checks if all char is lowercase
+# text.capitalize() # makes first letter uppercase
+# text.title() # makes first letter of each word uppercase
 
-
-
-print(text.count("l"))
+print(text.title())
