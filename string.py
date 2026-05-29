bbc25fd239d96078fba4d1aaff6c12c3b7719b13
incognito-dev07 <osasanolusola2007@gmail.com>
2026-05-29 11:34:44 +0100

    minor changes

---
+++ string.py
@@ -0,0 +1,9 @@
+greeting = "Hello world"
+
+# string slicing
+second_word = greeting[6:]
+step_slicing = greeting[0:5:2]
+print(step_slicing)
+
+reverse_second_word = second_word[::-1]
+print(reverse_second_word)
