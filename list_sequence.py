e7f2c73f916ec6f781bcddd5a2369198d486c610
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-02 15:52:02 +0100

    minor changes

--- list_sequence.py
+++ list_sequence.py
@@ -1,7 +1,8 @@
 fruits = ["apple","orange","mango","pawpaw"]
 
 list(fruits[1]) # converts a string to list
+len(fruits) # finds the length of a list
 
 
 
-print(list(fruits[1]))
+print(len(fruits[1]))
