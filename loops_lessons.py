d3fdb5d252426ff0132c37e2048bd7433a52b5f5
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-08 05:53:32 +0100

    minor fixes

--- loops_lessons.py
+++ loops_lessons.py
@@ -3,11 +3,16 @@ for i in range(1, 11):
   
 even_nums = list(range(0, 21, 2))
 print(even_nums)
-
 print("\n")
+
+
+# Enumerate Function
 usernames = ["Klaus", "Elijah", "Rebeccah", "Kol", "Freya", "Finn"]
 enums = list(enumerate(usernames))
 print(enums)
 
 for idx, name in enumerate(usernames, 1):
   print(f"{idx}. {name}")
+  
+  
+# Zip Function
