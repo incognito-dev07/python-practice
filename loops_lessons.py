17211790252df780fd26ec1e66949dd228082b08
incognito-dev07 <osasanolusola2007@gmail.com>
2026-06-08 05:51:57 +0100

    minor fixes

--- loops_lessons.py
+++ loops_lessons.py
@@ -1,11 +1,13 @@
-usernames = ["Klaus", "Elijah", "Rebeccah", "Kol", "Freya", "Finn"]
-
-for idx, name in enumerate(usernames, 1):
-  print(f"{idx}. {name}")
-  
-print("\n")
 for i in range(1, 11):
   print(i)
   
 even_nums = list(range(0, 21, 2))
 print(even_nums)
+
+print("\n")
+usernames = ["Klaus", "Elijah", "Rebeccah", "Kol", "Freya", "Finn"]
+enums = list(enumerate(usernames))
+print(enums)
+
+for idx, name in enumerate(usernames, 1):
+  print(f"{idx}. {name}")
